4acf714e2e26b55f309dc32fc702e95fffe13873
Your Name <root@Ethan.localdomain>
2023-02-03 15:10:49 -0800

    Created Header and boilerplate grid and keyboard components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './styles.css';
 
+import Header from './components/Header.js';
+import Grid from './components/Grid.js';
+import Keyboard from './components/Keyboard.js';
+
 
 export function App() {
   return (
-    <div className="text-slate-50">
-      <h1> Wordle Clone </h1>
+    <div>
+      <Header />
+      <Grid />
+      <Keyboard />
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header() {
+ return (
+  <h1 className="text-6xl font-bold text-white text-center py-5 bg-[#1a1919] shadow-md"> Wordle </h1>
+ )
+}
